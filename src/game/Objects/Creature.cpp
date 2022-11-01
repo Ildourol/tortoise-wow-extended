f075013e1038a2d4b29aaa7f41e8909366296db6
ratkosrb <ratkomladic2@abv.bg>
2022-11-01 02:33:43 +0200

    Prevent pets from assisting CallForHelp.
    Add explicit IsPet() check in Creature::CanAssistTo to prevent pets answering CallForHelp. (vmangos/core@8bdd87238)

--- src/game/Objects/Creature.cpp
+++ src/game/Objects/Creature.cpp
@@ -2361,7 +2361,7 @@ bool Creature::CanAssistTo(const Unit* u, const Unit* enemy, bool checkfaction /
         return false;
 
     // only free creature
-    if (GetCharmerOrOwnerGuid())
+    if (IsPet() || GetCharmerOrOwnerGuid())
         return false;
 
     // Invisible
