d8f4e56d2cf2b05f467f17e31fb6d98897f11047
ratkosrb <ratkomladic2@abv.bg>
2024-10-02 07:55:23 +0300

    Add end script for quest Cycle of Rebirth.
    Closes https://github.com/vmangos/core/issues/2785

--- src/game/Maps/ScriptCommands.cpp
+++ src/game/Maps/ScriptCommands.cpp
@@ -1439,7 +1439,7 @@ bool Map::ScriptCommand_RemoveGameObject(const ScriptInfo& script, WorldObject* 
     }
 
     pGo->SetLootState(GO_JUST_DEACTIVATED);
-    pGo->AddObjectToRemoveList();
+    pGo->Delete();
     return false;
 }
 
