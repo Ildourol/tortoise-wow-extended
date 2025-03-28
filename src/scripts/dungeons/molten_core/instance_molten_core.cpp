da2903c1a784912f37b1cd19b5b81e04fc38a457
Chaosvex <901914+Chaosvex@users.noreply.github.com>
2025-03-28 16:58:51 +0000

    Fix memory leak in molten core script (#2989)
    Handle instance data strings using member variable instead of leaking dynamically allocated string. (vmangos/core@340d88cb3)

--- src/scripts/dungeons/molten_core/instance_molten_core.cpp
+++ src/scripts/dungeons/molten_core/instance_molten_core.cpp
@@ -53,6 +53,8 @@ struct instance_molten_core : ScriptedInstance
 
     uint64 GOUseGuidList[7];
 
+    std::string strInstData;
+
     void Initialize() override
     {
         memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
@@ -385,9 +387,8 @@ struct instance_molten_core : ScriptedInstance
                    << RuneActive[2] << " " << RuneActive[3] << " " << RuneActive[4] << " "
                    << RuneActive[5] << " " << RuneActive[6];
 
-        // TODO: Fuite de memoire. Mais empeche un crash.
-        std::string* strInstData = new std::string(saveStream.str());
-        return strInstData->c_str();
+        strInstData = saveStream.str();
+        return strInstData.c_str();
     }
 
     uint32 GetData(uint32 uiType) override
