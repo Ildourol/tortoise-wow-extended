1757e919b97d96fe1965439a9edd16ded403edca
Chero <Chero@users.noreply.github.com>
2025-04-29 02:17:18 +0300

    Cancel immediately channeled spells that are about to fail due to no more valid targets.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -4326,7 +4326,7 @@ void Spell::update(uint32 difftime)
                 // check if there are alive targets left
                 if (!HasValidUnitPresentInTargetList())
                 {
-                    SendChannelUpdate(0);
+                    SendChannelUpdate(0, true);
                     finish();
                 }
 
