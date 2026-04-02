2dd7d2e99b9189e816e683293dc6b6d430e4a6c5
ratkosrb <ratkomladic2@abv.bg>
2026-04-02 11:35:04 +0300

    Only add bot to cache after adding to map.

--- src/game/PlayerBots/PlayerBotAI.cpp
+++ src/game/PlayerBots/PlayerBotAI.cpp
@@ -647,7 +647,6 @@ bool PlayerBotAI::SpawnNewPlayer(WorldSession* sess, uint8 class_, uint32 race_,
         return false;
     }
     newChar->Relocate(x, y, z, o);
-    sObjectMgr.InsertPlayerInCache(newChar);
     newChar->SetMap(map);
     newChar->SaveRecallPosition();
     newChar->CreatePacketBroadcaster();
@@ -660,6 +659,7 @@ bool PlayerBotAI::SpawnNewPlayer(WorldSession* sess, uint8 class_, uint32 race_,
         delete newChar;
         return false;
     }
+    sObjectMgr.InsertPlayerInCache(newChar);
     sess->SetPlayer(newChar);
     sess->SetMasterPlayer(mPlayer);
     sObjectAccessor.AddObject(newChar);
