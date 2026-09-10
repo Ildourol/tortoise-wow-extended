2dcdd9d69f4152dfd5de6282a80db9fc5adfe547
ratkosrb <ratkomladic2@abv.bg>
2026-09-10 15:37:47 +0200

    Fix potential memory leak when forming battleground groups.
    Thanks to Chero. Backport of VMaNGOS 1a4141090.

--- src/game/Battlegrounds/BattleGround.cpp
+++ src/game/Battlegrounds/BattleGround.cpp
@@ -1117,7 +1117,11 @@ void BattleGround::AddOrSetPlayerToCorrectBgGroup(Player *pPlayer, ObjectGuid pl
     {
         group = new Group;
         SetBgRaid(team, group);
-        group->Create(plr_guid, pPlayer->GetName());
+        if (!group->Create(plr_guid, pPlayer->GetName()))
+        {
+            SetBgRaid(team, nullptr);
+            delete group;
+        }
     }
 }
 
