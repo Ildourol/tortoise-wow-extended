a49660ae9d25df6a9b3c828d5859179f647b5cee
Ildourol <user@email.com>
2026-09-11 22:59:59 +0200

    playerbots: allow bot tank to move to 'spread' position only if they have aggro on their target (8d602912)

--- modules/mod-playerbots/src/playerbot/strategy/values/Stances.cpp
+++ modules/mod-playerbots/src/playerbot/strategy/values/Stances.cpp
@@ -190,6 +190,16 @@ namespace ai
     protected:
         WorldLocation GetLocationInternal() override
         {
+            if (ai->IsTank(bot))
+            {
+                Unit* target = GetTarget();
+
+                if (!target || target->GetVictim() != bot)
+                {
+                    return WorldLocation(bot->GetMapId(), bot->GetPositionX(), bot->GetPositionY(), bot->GetPositionZ());
+                }
+            }
+
             const float spreadDistance = 10.0f;
             const float spreadPadding = 0.5f;
             const float epsilon = 0.01f;
